95247b833330efca0e9871a776e2ee60c678a5fc
Abdullah <53242416+git-abdullah@users.noreply.github.com>
2023-02-25 14:45:38 +0500

    re-design everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./public/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-60': '#222831',
+        'primary-10': '#FFD369',
+        'primary-30': '#EEEEEE'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animate')
+  ],
 }
